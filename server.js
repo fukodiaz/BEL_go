4936e7ebfa1b861457cdefd4981773561e609e41
Ilya <fukodiaz@gmail.com>
2022-03-26 19:44:44 +0300

    deploying_5 deleted unnecessary files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults({
 	static: './dist'
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 server.use(middlewares);
 server.use(jsonServer.rewriter({
