6c2212a16e71ce20e3005d15732cf70694f2c15d
Fuko <fukodiaz@gmail.com>
2023-01-27 22:37:00 +0700

    change webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const conf = {
 	entry: './src/index.js',
 	output: {
-		path: path.resolve(__dirname),
+		path: path.resolve(__dirname,'dist'),
 		filename: 'main.js',
 		//publicPath: ''
 	},
