7d7a87f33fd47090ef8bca07150b5c25c9c33582
Fuko <fukodiaz@gmail.com>
2023-01-30 19:00:53 +0700

    server-app

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const clone = require('clone');
+//const server = jsonServer.create();
+//const router = jsonServer.router('db.json');
+const data = require('./db.json');
+
+const isProductionEnv = process.env.NODE_ENV === 'production';
+const server = jsonServer.create()
+
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
+	_isFake: isProductionEnv
+});
+
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3000;
@@ -9,6 +19,12 @@ server.use(middlewares);
 server.use(jsonServer.rewriter({
 	'/api/*': '/$1',
 }));
+
+server.use((req, res, next) => {
+	if (req.path !== '/')
+		router.db.setState(clone(data));
+		next();
+});
 server.use(router);
 
 server.listen(PORT, () => {
