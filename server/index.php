224b6dc31e37313ea179d0ee7ef654421294e0f9
Fuko <fukodiaz@gmail.com>
2023-08-19 13:26:02 +0700

    change package for envs in server part

--- server/index.php
+++ server/index.php
@@ -1,5 +1,6 @@
 <?php
-use Symfony\Component\Dotenv\Dotenv;
+//use Symfony\Component\Dotenv\Dotenv;
+use Dotenv\Dotenv;
 use DI\Container;
 use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
@@ -7,8 +8,10 @@ use Slim\Factory\AppFactory;
 //access to dependencies received via composer 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = new Dotenv();
-$dotenv -> load(__DIR__ . '/.env');
+// $dotenv = new Dotenv();
+// $dotenv -> load(__DIR__ . '/.env');
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 // build container for DI
 $containerBuilder = new ContainerBuilder();
