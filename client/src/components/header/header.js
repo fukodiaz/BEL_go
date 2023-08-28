b8d8971a5ad184cf48f9ebeaca1953d5ccfe54b6
Fuko <fukodiaz@gmail.com>
2023-08-28 21:23:57 +0700

    correct styles in header

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import FilterCities from '../filter-cities';
@@ -11,14 +11,9 @@ import imgTab from './back-main-tablet.jpg';
 import imgDesk from './back-main-desktop.jpg';
 
 function Header() {
-
-	useEffect(() => {
-
-	});
 		
 	return (
-		<header className={styles.mainheader}>
-
+		<header className={styles.mainHeader}>
 			<picture>
 					<source media="(min-width: 1280px)" srcSet={imgDesk} />
 					<source media="(min-width: 768px)" srcSet={imgTab} />
